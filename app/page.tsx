b4d8387c5385be1cecb085db15d3d64c508ba96f
Alexandru Moisescu <amoisescu@machinas.com>
2024-08-21 12:00:56 +0300

    feat(app): first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <main>
+            <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+            <h1>Welcome to this NextJS Course!</h1>
+            <p>🔥 Let&apos;s get started! 🔥</p>
+            <p>🔥 Let&apos;s get started! 🔥</p>
+            <p>🔥 Let&apos;s get started! 🔥</p>
+            <p>🔥 Let&apos;s get started! 🔥</p>
+            <p>🔥 Let&apos;s get started! 🔥</p>
+        </main>
+    );
+  }
